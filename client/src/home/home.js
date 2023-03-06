44ab9be3379b3b13b89baa12602221a6a1a5848b
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-03-06 23:54:59 +0900

    revise Home style

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
-import { View,Text,ScrollView } from 'react-native';
+import { View,Text,ScrollView,TouchableOpacity } from 'react-native';
 
 import { template } from "../styles/template/page";
+import {styles} from "../styles/screens/home/home";
+
 
 class Home extends Component {
     constructor(props) {
         super(props);
     }
-  
+    goTrailScreen=()=>{
+        this.props.navigation.navigate('GetTrails');
+    }
+    goCommunityScreen=()=>{
+        this.props.navigation.navigate('Community');
+    }
     render() {
         return (
         <View style={template.total_container}>
-            <Text>홈</Text>
+            <View style={styles.headerView}><Text>OneStep</Text></View>
+            <ScrollView>
+            <View style={styles.profileView}>
+
+            </View>
+            <View style={styles.trailTopView}>
+                <View style={styles.titleView}>
+                    <View style={{flex:4}}>
+                    <Text style={template.titleText}>추천 산책로</Text>
+                    </View>
+                    <View style={{flex:1}}>
+                        <TouchableOpacity onPress={this.goTrailScreen}><Text>전체보기</Text></TouchableOpacity>
+                    </View>
+                </View>
+                
+            </View>
+            <View style={styles.communityTopView}>
+                <View style={styles.titleView}>
+                    <View style={{flex:4}}>
+                        <Text style={template.titleText}>인기글 Top5</Text>
+                    </View>
+                    <View style={{flex:1}}>
+                        <TouchableOpacity onPress={this.goCommunityScreen}><Text>전체보기</Text></TouchableOpacity>
+                    </View>
+                </View>
+                
+            </View>
+            </ScrollView>
        </View>
         );
     }
