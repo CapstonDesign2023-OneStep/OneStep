3f124c5c5411911863e9eb375ae6fc478c9c545f
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-03-29 22:05:03 +0900

    글작성 디자인 틀 구성

--- client/src/community/write_community.js
+++ client/src/community/write_community.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View,Text,ScrollView } from 'react-native';
+import { View,Text,ScrollView, TouchableOpacity } from 'react-native';
 
 import { template } from "../styles/template/page";
-
+import {styles} from "../styles/screens/community/community";
 class WriteCommunity extends Component {
     constructor(props) {
         super(props);
@@ -10,8 +10,13 @@ class WriteCommunity extends Component {
   
     render() {
         return (
-            <View style={template.total_container}>
-           
+        <View style={template.total_container}>
+           <View style={styles.writeView}>
+          
+           </View>
+           <View style={styles.buttonView}>
+                <TouchableOpacity style={template.button}><Text style={template.buttonText}>등록하기</Text></TouchableOpacity>
+           </View>
        </View>
         );
     }
